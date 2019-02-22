85d1ebc90ed15115ee8041f2f01bf20df0d10a6a
Benjamin Tovar <btovar@nd.edu>
2019-02-22 08:02:28 -0600

    fix example_02 to match slides

--- work_queue/example_02/example_02.py
+++ work_queue/example_02/example_02.py
@@ -17,10 +17,7 @@ print 'WorkQueue on port: {}'.format(q.port)
 q.enable_monitoring()
 
 # create a category for all tasks
-q.specify_category_max_resources('my-tasks', {'cores': 1})
-
-# some resources were specified: memory, and disk will be labeled
-# automatically.
+q.specify_category_max_resources('my-tasks', {'cores': 1, 'disk': 500})
 q.specify_category_mode('my-tasks', WQ.WORK_QUEUE_ALLOCATION_MODE_MAX_THROUGHPUT)
 
 # create 30 tasks. Each task simply creates a 200MB file, using 10MB of memory
@@ -32,16 +29,21 @@ for i in range(0,30):
     t.specify_max_retries(2)
     q.submit(t)
 
+# create a task that will break the limits set
+t = WQ.Task('python task.py 1000')
+t.specify_input_file('task.py', cache = True)
+t.specify_category('my-tasks')
+t.specify_max_retries(2)
+q.submit(t)
+
+
 # wait for task to finish
 while not q.empty():
     t = q.wait(60)
     if t:
         print 'task {} finished:'.format(t.id)
-        if t.result == WQ.WORK_QUEUE_RESULT_SUCCESS:
-            print 'allocated mem {} disk {}, used mem {} disk {}'.format(t.resources_allocated.memory,
-                                                                         t.resources_allocated.disk,
-                                                                         t.resources_measured.memory,
-                                                                          t.resources_measured.disk)
-        else:
-            print 'failed with code {}'.format(t.return_status)
+	print 'allocated mem {} disk {}, used mem {} disk {}'.format(t.resources_allocated.memory,
+                                                                     t.resources_allocated.disk,
+                                                                     t.resources_measured.memory,
+                                                                     t.resources_measured.disk)
 
